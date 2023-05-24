3371b20b38f4cc5a444c1f07597a2f1ffa59f9dc
Ronan Fourneuve <65066876+ronanfrnv@users.noreply.github.com>
2023-05-24 09:06:17 +0200

    Add files via upload

---
+++ lecteurY.ps1
@@ -0,0 +1,11 @@
+﻿$driveLetter = "Y"
+$networkPath = ""
+
+$drive = Get-WmiObject -Class Win32_LogicalDisk | Where-Object {$_.DeviceID -eq $driveLetter}
+
+if (!$drive) {
+    New-PSDrive -Name $driveLetter -PSProvider FileSystem -Root $networkPath
+    Write-Host "Le lecteur Y a été monté avec succès."
+} else {
+    Write-Host "Le lecteur Y est déjà monté."
+}
